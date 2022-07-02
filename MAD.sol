c1cf3ab874957f859dcabd886257319857a2a2c3
Chris Paulino <chris1026.cp@gmail.com>
2022-07-02 12:45:24 -0400

    Create MAD.sol

---
+++ MAD.sol
@@ -0,0 +1,224 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.4.26; 
+
+//---------------------------------------------
+// Deployed to : DEPLOY-ADDRESS-HERE
+//          tokens deployed to address: DEPLOY-ADDRESS-HERE
+//
+//
+//---------------------------------------------
+
+//---------------------------------------------
+//Safe Math
+//---------------------------------------------
+contract SafeMath{
+    function safeAdd(uint a, uint b) public pure returns(uint c){
+        c = a + b;
+        require(c >= a);
+
+    }
+    function safeSub(uint a, uint b) public pure returns (uint c){
+        require(b <= a);
+        c = a - b;
+    }
+
+    function safeMul(uint a, uint b) public pure returns (uint c){
+        c = a * b;
+        require (a == 0 || c / a == b);
+
+    }
+
+    function safeDiv(uint a, uint b) public pure returns (uint c){
+        require(b > 0);
+        c = a / b;
+    }
+}
+
+/* ERC20 token standard*/
+contract ERC20Interface{
+    function totalSuppyly() public constant returns (uint);
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+    function transfer(address to, uint tokens) public returns(bool success);
+    function approve (address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns(bool success);
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+
+
+}
+
+ // -------------------------------------------------------
+ // Contract function to recevice approval and execute function in one call 
+ //
+ // Borrowed from MiniMeToken's Github
+ // -------------------------------------------------------
+contract ApproveAndCallFallBack {
+    function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
+
+}
+
+ // -------------------------------------------------------
+ // Owned contact
+ // -------------------------------------------------------
+contract Owned {
+    address public owner;
+    address public newOwner;
+
+    event OwnershipTransferred(address indexed _from, address indexed _to);
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner{
+        require(msg.sender == owner);
+        _;
+    }
+
+    function transferOwnership(address _newOwner) public onlyOwner{
+        newOwner = _newOwner;
+    }
+
+    function acceptOwnership() public {
+        require(msg.sender == newOwner);
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+        newOwner = address(0);
+    }
+
+}
+
+
+
+ // -------------------------------------------------------
+ // This is the main contract the meat and potatoes 
+ //ERC20 Token, with the addition of symbol, name and decimals and assisted
+ // token transfer
+ // -------------------------------------------------------
+contract MAD is ERC20Interface, SafeMath, Owned {
+    string public symbol;
+    string public name;
+    uint8 public decimals;
+    uint public _totalSupply;
+
+    mapping (address => uint) balances;
+    mapping (address => mapping(address => uint)) allowed; 
+
+
+
+    // ---------------------------------------------
+    // Constructor
+    // ---------------------------------------------
+
+    constructor() public {
+        symbol = "MAD";
+        name = "MADDNESS Token";
+        decimals = 18;
+        _totalSupply = 10000000000000000000000000000;
+        //address(key) => value
+        balances[DEPLOY-ADDRESS-HERE] = _totalSupply; 
+        emit Transfer(address(0), DEPLOY-ADDRESS-HERE, _totalSupply);
+    }
+
+    // ---------------------------------------------
+    // Total Supply
+    // ---------------------------------------------
+    function totalSupply() public constant returns(uint){
+        return _totalSupply - balances[address(0)];
+    }
+
+
+    // ---------------------------------------------------------------
+    // Get the token balance for account tokenOwner
+    // ---------------------------------------------------------------
+    function balanceOf(address tokenOwner) public constant returns (uint balance){
+        return balances[tokenOwner];
+    }
+
+
+    // ---------------------------------------------------------------
+    // Transfer the banlance from token owner's account to anccount 
+    // - owner's account must have sufficient balance to transfer
+    // - 0 valuce transfers are allowed 
+    // ---------------------------------------------------------------
+    function transfer(address to, uint tokens) public returns (bool success){ 
+        balances[msg.sender] = safeSub(balances[msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
+
+
+    //recommends that there are no checks for the approval double-spend attack
+    // as this should be implemented in user interfaces
+    // -------------------------------------------------------
+    function approve(address spender, uint tokens) public returns (bool success){
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+
+
+    // -------------------------------------------------------
+    //Transfer tokens from the account to the to account
+    //
+    // The calling account must already have sufficient tokens approve(..) -d
+    //for spending from the from account and 
+    // - From account must have sufficient balance to transfer
+    // - Spender must have sufficient allowance to transfer
+    // - 0 value transfer are allowed
+    // ----------------------------------------------------------
+    
+    // this is different from transfer function because we are dealing with third party application(Ex. Uniswap). 
+    function transferFrom(address from, address to, uint tokens) public returns (bool sucesss){ 
+        balances[from] = safeSub(balances[from], tokens);
+        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
+        balances[to] = safeAdd(balances[to], tokens);
+        emit Transfer(from, to, tokens);
+        return true;
+    
+    } 
+    
+    // -------------------------------------------------------------------
+    // Returns the amount of tokens approved by the owner that can be 
+    // transferred to the spender's account 
+    // ------------------------------------------------------------------- 
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining){
+        return allowed[tokenOwner][spender];
+
+    }
+
+    // -------------------------------------------------------------------
+    // Execute when the contract is launched. 
+    // Token owner can approve for spender to transferFrom(...) tokens
+    // from the token owner's account. The spender contract function 
+    // receiveApproval(...) is then executed
+    // -------------------------------------------------------------------
+    function approveAndCall(address spender, uint tokens, bytes data) public returns(bool success){
+        allowed[msg.sender][spender] = tokens; //access both the person calling the function and spender through the allowed mapping
+        emit Approval(msg.sender, spender, tokens);
+        
+        // This line calls the ApproveAndCallFallBack smart contract
+        ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
+        return true; 
+    }
+
+
+    // -------------------------------------------------------
+    // Don't accept ETH
+    // -------------------------------------------------------
+    function() public payable {
+        revert();
+    }
+
+    // -------------------------------------------------------
+    //Owner can transfer out any accidentally sent ERC20 tokens 
+    // -------------------------------------------------------
+    function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner returns (bool success){
+        return ERC20Interface(tokenAddress).transfer(owner, tokens);
+    }
+
+}
+
